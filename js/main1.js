d9de38bc275502827c48a4a200781e363012d24c
huangjuntu <1721053474@qq.com>
2016-10-21 17:34:28 +0800

    右侧局部样式--修改

--- js/main1.js
+++ js/main1.js
@@ -34,29 +34,48 @@
 $('#mySelect').change(function () {
     var select_value = $(this).children('option:selected').val()
     if (select_value == 1) {
+    	$('.desktop-two-button').removeAttr("disabled");
         $('.desktop-two-center-one').css("display", "block");
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-three').css("display", "none");
         $('.desktop-two-center-four').css("display", "none");
+        
+        $(".desktop-two-button").removeClass("white_bd1");
     } else if (select_value == 2) {
         $('.desktop-two-center-two').css("display", "block");
         $('.desktop-two-center-one').css("display", "none");
         $('.desktop-two-center-three').css("display", "none");
         $('.desktop-two-center-four').css("display", "none");
+        $(".desktop-two-center .news").css("display","block");
+        $('.desktop-two-button').attr("disabled","disabled");
+        $(".desktop-two-button").addClass("white_bd1");
     } else if (select_value == 3) {
         $('.desktop-two-center-three').css("display", "block");
         $('.desktop-two-center-one').css("display", "none");
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-four').css("display", "none");
+        $(".desktop-two-button").addClass("white_bd1");
     } else if (select_value == 4) {
         $('.desktop-two-center-four').css("display", "block");
         $('.desktop-two-center-one').css("display", "none");
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-three').css("display", "none");
+        $(".desktop-two-button").addClass("white_bd1");
     } else {}
 })
+
+
 $(".desktop-two-button").click(function () {
+	$(".desktop-two-button").addClass("white_bd");
+			setTimeout(function() {
+				$(".desktop-two-button").removeClass("white_bd");
+			}, 100);
     $(".desktop-two-center-one .news").css("display", "none");
+    $(".desktop-two-center-two .news").css("display", "none");
+			$(".desktop-two-center-three .news").css("display", "none");
+			$(".desktop-two-center-four .news").css("display", "none");
+			$(".desktop-two-center-five .news").css("display", "none");
+			
     $(".shalou").css("display", "block");
     $(".show-pic").css("display", "none");
     setTimeout(function () {
@@ -67,11 +86,38 @@ $(".desktop-two-button").click(function () {
     }, 5000);
     clearTimeout();
 })
-$(".shalou").mouseover(function () {
-    $(".code-img").css("display", "block");
-}).mouseleave(function () {
-    $(".code-img").css("display", "none");
+
+
+//黑色区域
+var timer2 = 0;
+var imgs = ['img/zhbc.gif', 'img/zhbc1.gif', 'img/zhbc2.gif']; //注意：换成三张名称一样的不行
+var len = imgs.length;
+var current = 0;
+$(".shalou").live('mouseover', function() {
+	current = ++current; //等同于下面注释部分
+	if(current == len) {
+		current = 0;
+	} else {
+		current = current;
+	}
+	//current = ++current == len ? 0: current;
+	$("#target").attr("src", imgs[current]);
+	$(".code-img").css("display", "block");
+	if(!timer2) {
+		timer2 = setTimeout(function() {
+			$(".code-img").css("display", "none");
+		}, 2000);
+	}
+
+}).mouseleave(function() {
+	clearTimeout(timer2); //最好有这一句
+	timer2 = null;
+	$(".code-img").css("display", "none");
 });
+
+//
+//
+
 
 // 菜单栏按钮点击
 $('.jq22').css("display","none");
