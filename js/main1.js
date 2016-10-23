430b434ac6282c106b6b15bb29e0017d3e6c7f69
hjt <huangjuntuu@gmail.com>
2016-10-23 20:18:55 +0800

    右侧select--修改

--- js/main1.js
+++ js/main1.js
@@ -39,6 +39,7 @@ $('#mySelect').change(function () {
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-three').css("display", "none");
         $('.desktop-two-center-four').css("display", "none");
+        $('.desktop-two-center-five').css("display", "none");
         
         $(".desktop-two-button").removeClass("white_bd1");
     } else if (select_value == 2) {
@@ -48,18 +49,28 @@ $('#mySelect').change(function () {
         $('.desktop-two-center-four').css("display", "none");
         $(".desktop-two-center .news").css("display","block");
         $('.desktop-two-button').attr("disabled","disabled");
+        $('.desktop-two-center-five').css("display", "none");
         $(".desktop-two-button").addClass("white_bd1");
     } else if (select_value == 3) {
         $('.desktop-two-center-three').css("display", "block");
         $('.desktop-two-center-one').css("display", "none");
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-four').css("display", "none");
+        $('.desktop-two-center-five').css("display", "none");
         $(".desktop-two-button").addClass("white_bd1");
     } else if (select_value == 4) {
         $('.desktop-two-center-four').css("display", "block");
         $('.desktop-two-center-one').css("display", "none");
         $('.desktop-two-center-two').css("display", "none");
         $('.desktop-two-center-three').css("display", "none");
+        $('.desktop-two-center-five').css("display", "none");
+        $(".desktop-two-button").addClass("white_bd1");
+    } else if (select_value == 5) {
+        $('.desktop-two-center-five').css("display", "block");
+        $('.desktop-two-center-one').css("display", "none");
+        $('.desktop-two-center-two').css("display", "none");
+        $('.desktop-two-center-three').css("display", "none");
+        $('.desktop-two-center-four').css("display", "none");
         $(".desktop-two-button").addClass("white_bd1");
     } else {}
 })
@@ -72,9 +83,9 @@ $(".desktop-two-button").click(function () {
 			}, 100);
     $(".desktop-two-center-one .news").css("display", "none");
     $(".desktop-two-center-two .news").css("display", "none");
-			$(".desktop-two-center-three .news").css("display", "none");
-			$(".desktop-two-center-four .news").css("display", "none");
-			$(".desktop-two-center-five .news").css("display", "none");
+	$(".desktop-two-center-three .news").css("display", "none");
+	$(".desktop-two-center-four .news").css("display", "none");
+	$(".desktop-two-center-five .news").css("display", "none");
 			
     $(".shalou").css("display", "block");
     $(".show-pic").css("display", "none");
